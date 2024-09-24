e04cbb933c62f801d7fbb22a2ce9eb75e216a900
Edilson-EJ <edilson.franca.melo@gmail.com>
2024-09-24 08:48:15 -0300

    teste

--- Sintaxe e Tipos de Dados em C#/obj/Debug/net8.0/Sintaxe e Tipos de Dados em C#.AssemblyInfo.cs
+++ Sintaxe e Tipos de Dados em C#/obj/Debug/net8.0/Sintaxe e Tipos de Dados em C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sintaxe e Tipos de Dados em C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d4d30baf297788b8908934e2d8a62231ce0ebd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+999b2ec1683dc503b22b7459a3721a117b4dfcc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sintaxe e Tipos de Dados em C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sintaxe e Tipos de Dados em C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
