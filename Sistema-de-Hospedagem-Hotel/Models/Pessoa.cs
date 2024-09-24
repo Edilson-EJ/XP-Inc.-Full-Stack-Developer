56ac625ce6602c0dae52e0128a9225dd686f899a
Edilson-EJ <edilson.franca.melo@gmail.com>
2024-09-24 10:14:42 -0300

    desafio de hospedagem

---
+++ Sistema-de-Hospedagem-Hotel/Models/Pessoa.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Hospedagem_Hotel.Models
+{
+    public Pessoa() { }
+
+    public Pessoa(string nome)
+    {
+        Nome = nome;
+    }
+
+    public Pessoa(string nome, string sobrenome)
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
+    }
+
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+}
