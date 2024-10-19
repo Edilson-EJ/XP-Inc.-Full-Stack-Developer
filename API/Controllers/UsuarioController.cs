0f0b610ce38005433ed985019c5a2b89001b1c4c
Edilson-EJ <edilson.franca.melo@gmail.com>
2024-10-19 16:26:06 -0300

    iniciando a API

---
+++ API/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("Controler")]
+    public class UsuarioController : ControllerBase 
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }
+    }
+}
